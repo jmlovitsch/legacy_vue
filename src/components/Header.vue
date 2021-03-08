9f6801b700ee18ce09cdd16a49b5e4d1b1430528
john <jmlovitsch@gmail.com>
2021-03-07 18:35:22 -0600

    tab change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-nav tabs>
-      <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled><b>Quick Game App</b></b-nav-item>
       <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
     </b-nav>
   </div>
